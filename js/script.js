c18aa31afe690479ede414fb108b1654f3da67c6
farrelfernaldypz-lgtm <farrelfernaldypz@gmail.com>
2026-02-13 15:46:40 +0700

    memperbaiki keseluruhan website dan finalisasi
    merubah sebagian besar dari html, css, dan java script sekaligus finalisasi

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-const navbar = document.getElementById("navbar");
+const handleAllEvents = () => {
+    const reveals = document.querySelectorAll(".reveal");
+    reveals.forEach((element) => {
+        const windowHeight = window.innerHeight;
+        const elementTop = element.getBoundingClientRect().top;
+        if (elementTop < windowHeight - 100) {
+            element.classList.add("active");
+        }
+    });
 
-window.addEventListener("scroll", () => {
+    const navbar = document.getElementById("navbar");
     if (window.scrollY > 50) {
         navbar.classList.add("nav-scrolled");
     } else {
         navbar.classList.remove("nav-scrolled");
     }
-});
+};
+
+window.addEventListener("scroll", handleAllEvents);
+window.addEventListener("load", handleAllEvents);
